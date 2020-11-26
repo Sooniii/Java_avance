7c38bbde682ffaf8e720e3f588502c05044471b7
Sooniii <theodulejanchen@hotmail.fr>
2020-11-26 19:06:18 +0100

    Menu gestion hopitaux presque fini

--- src/com/company/Medicament.java
+++ src/com/company/Medicament.java
@@ -1,6 +1,11 @@
 package com.company;
 
+import javax.swing.text.Style;
+import java.util.Scanner;
+
 public class Medicament {
+
+    static Scanner sc = new Scanner(System.in);
 
     String codeISN;
     String nomenclature;
@@ -16,5 +21,48 @@ public class Medicament {
         this.quantite = quantite;
         this.prescriptionDate = prescriptionDate;
         this.prix = prix;
+    }
+
+    public static void afficherListe(){
+        for (int i = 0; i < ListManager.medicamentList.size(); i++){
+            System.out.print((i + 1) + " : " +ListManager.medicamentList.get(i).codeISN + ", ");
+            System.out.print(ListManager.medicamentList.get(i).nomenclature + ", ");
+            System.out.print(ListManager.medicamentList.get(i).description + ", ");
+            System.out.print(ListManager.medicamentList.get(i).quantite + ", ");
+            System.out.print(ListManager.medicamentList.get(i).prescriptionDate + ", ");
+            System.out.println(ListManager.medicamentList.get(i).prix);
+        }
+        System.out.println(" ");
+    }
+
+    public static void add(){
+        System.out.print("Nom du médicament : ");
+        String nom = sc.next();
+        System.out.print("Code ISN : ");
+        String isn = sc.next();
+        System.out.print("Description : ");
+        String description = sc.next();
+        System.out.print("Quantité : ");
+        String quantite = sc.next();
+        System.out.print("Date de prescription : ");
+        String prescriptionDate = sc.next();
+        System.out.print("Prix : ");
+        String prix = sc.next();
+        ListManager.add(new Medicament(isn, nom, description, quantite, prescriptionDate, prix));
+        System.out.println("Le médicament a bien été ajouté");
+        System.out.println(" ");
+    }
+
+    public static void delete(){
+        System.out.println("Quel médicament voulez vous supprimer ?");
+        afficherListe();
+        String user = sc.next();
+        int userN = Integer.parseInt(user);
+        while ((userN) > ListManager.medicamentList.size()) {
+            System.out.println("Ce médicament n'existe pas");
+            user = sc.next();
+            userN = Integer.parseInt(user);
+        }
+        ListManager.medicamentList.remove(userN - 1);
     }
 }
--- src/com/company/Praticien.java
+++ src/com/company/Praticien.java
@@ -1,6 +1,10 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Praticien{
+
+    static Scanner sc = new Scanner(System.in);
 
     String nom;
     String prenom;
@@ -18,5 +22,76 @@ public class Praticien{
         this.grade = grade;
         this.tauxHoraire = tauxHoraire;
         this.hopital = hopital;
+    }
+
+    public static void afficherListe(){
+        for (int i = 0; i < ListManager.praticienList.size(); i++){
+            System.out.print((i + 1) + " : " +ListManager.praticienList.get(i).nom + ", ");
+            System.out.print(ListManager.praticienList.get(i).prenom + ", ");
+            System.out.print(ListManager.praticienList.get(i).matricule + ", ");
+            System.out.print(ListManager.praticienList.get(i).specialite + ", ");
+            System.out.print(ListManager.praticienList.get(i).grade + ", ");
+            System.out.print(ListManager.praticienList.get(i).tauxHoraire + ", ");
+            System.out.println(ListManager.praticienList.get(i).hopital.nom);
+        }
+        System.out.println(" ");
+    }
+
+    public static void add(){
+        System.out.print("Nom du praticien : ");
+        String nom = sc.next();
+        System.out.print("Prénom : ");
+        String prenom = sc.next();
+        System.out.print("Matricule : ");
+        String matricule = sc.next();
+        System.out.print("Spécialité (Cardiologue, ORL, Rhumatologue) : ");
+        String specialite = sc.next();
+        while (!(specialite.equals("Cardiologue") || specialite.equals("ORL") || specialite.equals("Rhumatologue"))) {
+            specialite = sc.next();
+        }
+        System.out.print("Grade (Professeur, Specialiste, Medecin) : ") ;
+        String grade = sc.next();
+        while (!(grade.equals("Professeur") || grade.equals("Specialiste") || grade.equals("Medecin"))) {
+            grade = sc.next();
+        }
+        System.out.print("Taux Horaires : ");
+        String tauxHoraire = sc.next();
+        System.out.print("Lite des hopitaux disponibles : ");
+        for (int i = 0; i < ListManager.hopitalList.size(); i++){
+            System.out.print(ListManager.hopitalList.get(i).nom + " ");
+        }
+        System.out.println(" ");
+        System.out.print("Hopital : ");
+        String hopitalName = sc.next();
+        int index = 0;
+        boolean goodName = false;
+        while (!goodName) {
+            for (int i = 0; i < ListManager.hopitalList.size(); i++) {
+                if (hopitalName.equals(ListManager.hopitalList.get(i).nom)) {
+                    goodName = true;
+                    index = i;;
+                }
+            }
+            if (goodName) break;
+            System.out.println("Cet hopital n'existe pas");
+            hopitalName = sc.next();
+        }
+        Hopital hopitalP = ListManager.hopitalList.get(index);
+        ListManager.add(new Praticien(nom, prenom, matricule, specialite, grade, tauxHoraire, hopitalP));
+        System.out.println("Le praticien a bien été ajouté");
+        System.out.println(" ");
+    }
+
+    public static void delete(){
+        System.out.println("Quel praticien voulez vous supprimer ?");
+        afficherListe();
+        String user = sc.next();
+        int userN = Integer.parseInt(user);
+        while ((userN) > ListManager.praticienList.size()) {
+            System.out.println("Ce praticien n'existe pas");
+            user = sc.next();
+            userN = Integer.parseInt(user);
+        }
+        ListManager.praticienList.remove(userN - 1);
     }
 }
