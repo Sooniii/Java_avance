01df91088ad21515dfdc389b3f49dc2fba310edf
Daizu <f.keltoumi@gmail.com>
2020-11-27 11:42:40 +0100

    Jeu ColossalCave

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import com.company.Game.ColossalCave;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        }
+        ColossalCave.regle();
+
     }
+}
+
 
